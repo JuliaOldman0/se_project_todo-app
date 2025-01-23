7da8d23f9014e86708ffb045d42c41c6fce7e05b
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2025-01-22 19:51:56 -0500

    finished part 3

---
+++ components/Popup.js
@@ -0,0 +1,10 @@
+class Popup {
+  constructor({ popupSelect }) {
+    this.popupElement = document.querySelector(popupSelect);
+    console.log(this.popupElement);
+  }
+
+  
+}
+
+export default Popup;
