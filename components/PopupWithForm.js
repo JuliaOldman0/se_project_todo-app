a530f4d1795a7a7bc4eda6e1503c4bb35316e1d7
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2025-01-26 09:01:33 -0500

    saving work

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,9 +1,15 @@
 import Popup from "./Popup.js";
 
 class PopupWithForm extends Popup {
-constructor ({popupSelector, handleFormSubmit}) {
-    super({popupSelector});
-}
+  constructor({ popupSelector, handleFormSubmit }) {
+    super({ popupSelector });
+    this._popupForm =  this_popupElement.querySelector(".popup__form");
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupForm.addEventListener("submit", (evt) => {});
+  }
 }
 
 export default PopupWithForm;
